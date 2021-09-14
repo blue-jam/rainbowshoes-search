e15f71c32f1e6ab693674b27ca1c8c27b0f8fab7
blue_jam <blue-jam@users.noreply.github.com>
2021-09-14 20:27:06 +0900

    Create rainbowshoes-search with SpringInitlzr

---
+++ src/main/kotlin/moe/rainbowshoes/search/RainbowshoesSearchApplication.kt
@@ -0,0 +1,11 @@
+package moe.rainbowshoes.search
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RainbowshoesSearchApplication
+
+fun main(args: Array<String>) {
+	runApplication<RainbowshoesSearchApplication>(*args)
+}
