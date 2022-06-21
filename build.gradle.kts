755ad4187eeae9ae26e1d11cd1973de1d556e9d2
blue_jam <blue-jam@users.noreply.github.com>
2022-06-21 11:29:35 +0900

    Remove vulnerable libs from dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.5.13"
+	id("org.springframework.boot") version "2.6.8"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.5.21"
 	kotlin("plugin.spring") version "1.5.21"
@@ -33,8 +33,22 @@ dependencies {
 	implementation("org.apache.lucene:lucene-queryparser:8.2.0")
 	implementation("org.codelibs:lucene-analyzers-kuromoji-ipadic-neologd:8.2.0-20200120")
 
-	implementation("com.norconex.collectors:norconex-collector-http:3.0.0")
+	implementation("com.norconex.collectors:norconex-collector-http:3.0.0") {
+		exclude("com.h2database", "h2")
+		exclude("com.github.junrar", "junrar")
+		exclude("com.google.guava", "guava")
+		exclude("com.google.protobuf", "protobuf-java")
+		exclude("com.opencsv", "opencsv")
+		exclude("io.netty", "netty-all")
+		exclude("org.apache.commons", "commons-compress")
+		exclude("org.apache.poi", "poi")
+		exclude("org.apache.poi", "poi-scratchpad")
+	}
+	implementation("com.h2database:h2:2.1.212")
 	implementation("edu.ucar:jj2000:5.2")
+	implementation("org.jsoup:jsoup:1.14.3")
+	implementation("com.google.guava:guava:30.1.1-jre")
+	implementation("org.apache.commons:commons-compress:1.21")
 
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
